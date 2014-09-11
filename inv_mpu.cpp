cebd76ed2798d1027851c1876b099c2d3dd52a1e
gregd72002 <gregd72002@gmail.com>
2014-09-11 17:12:01 +0100

    fixing MPU6500/9250 support

--- inv_mpu.cpp
+++ inv_mpu.cpp
@@ -405,6 +405,7 @@ int mpu_select_device(int device)
   return 0;
 }
 
+#if defined MPU6050
 void mpu_init_structures()
 {
     reg->who_am_i         = 0x75;
@@ -485,6 +486,91 @@ void mpu_init_structures()
     st->hw = hw;
     st->test = test;
 };
+#elif defined MPU6500
+void mpu_init_structures()
+{
+    reg->who_am_i         = 0x75;
+    reg->rate_div         = 0x19;
+    reg->lpf              = 0x1A;
+    reg->prod_id          = 0x0C;
+    reg->user_ctrl        = 0x6A;
+    reg->fifo_en          = 0x23;
+    reg->gyro_cfg         = 0x1B;
+    reg->accel_cfg        = 0x1C;
+    reg->accel_cfg2        = 0x1D;
+    reg->lp_accel_odr     = 0x1E;
+    reg->motion_thr       = 0x1F;
+    reg->motion_dur       = 0x20;
+    reg->fifo_count_h     = 0x72;
+    reg->fifo_r_w         = 0x74;
+    reg->raw_gyro         = 0x43;
+    reg->raw_accel        = 0x3B;
+    reg->temp             = 0x41;
+    reg->int_enable       = 0x38;
+    reg->dmp_int_status   = 0x39;
+    reg->int_status       = 0x3A;
+    reg->accel_intel      = 0x69;
+    reg->pwr_mgmt_1       = 0x6B;
+    reg->pwr_mgmt_2       = 0x6C;
+    reg->int_pin_cfg      = 0x37;
+    reg->mem_r_w          = 0x6F;
+    reg->accel_offs       = 0x77;
+    reg->i2c_mst          = 0x24;
+    reg->bank_sel         = 0x6D;
+    reg->mem_start_addr   = 0x6E;
+    reg->prgm_start_h     = 0x70;
+#ifdef AK89xx_SECONDARY
+    reg->raw_compass      = 0x49;
+    reg->yg_offs_tc       = 0x01;
+    reg->s0_addr          = 0x25;
+    reg->s0_reg           = 0x26;
+    reg->s0_ctrl          = 0x27;
+    reg->s1_addr          = 0x28;
+    reg->s1_reg           = 0x29;
+    reg->s1_ctrl          = 0x2A;
+    reg->s4_ctrl          = 0x34;
+    reg->s0_do            = 0x63;
+    reg->s1_do            = 0x64;
+    reg->i2c_delay_ctrl   = 0x67;
+#endif
+    switch (deviceIndex) {
+      case 0:
+        hw->addr = 0x68;
+        break;
+
+      case 1:
+        hw->addr = 0x69;
+        break;
+    }
+    hw->max_fifo          = 1024;
+    hw->num_reg           = 128;
+    hw->temp_sens         = 321;
+    hw->temp_offset       = 0;
+    hw->bank_size         = 256;
+#if defined AK89xx_SECONDARY
+    hw->compass_fsr      = AK89xx_FSR;
+#endif
+
+    test->gyro_sens      = 32768/250;
+    test->accel_sens     = 32768/16;
+    test->reg_rate_div   = 0;    /* 1kHz. */
+    test->reg_lpf        = 1;    /* 188Hz. */
+    test->reg_gyro_fsr   = 0;    /* 250dps. */
+    test->reg_accel_fsr  = 0x18; /* 16g. */
+    test->wait_ms        = 50;
+    test->packet_thresh  = 5;    /* 5% */
+    test->min_dps        = 10.f;
+    test->max_dps        = 105.f;
+    test->max_gyro_var   = 0.14f;
+    test->min_g          = 0.3f;
+    test->max_g          = 0.95f;
+    test->max_accel_var  = 0.14f;
+
+    st->reg = reg;
+    st->hw = hw;
+    st->test = test;
+};
+#endif
 
 #define MAX_PACKET_LENGTH (12)
 
